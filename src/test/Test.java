b190b7251e49ba3ba0ce67c260ac84650d5f7ce0
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2017-10-14 16:51:06 +0200

    configurazione main e aggiunta file graph.txt per i test

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,29 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import calculator.SimCalculator;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			FileReader file= new FileReader(args[0]);
+			BufferedReader b= new BufferedReader(file);
+			String s=b.readLine();
+			SimCalculator calculator=new SimCalculator();
+			while(s!=null) {
+				calculator.initStructures(s);
+				s=b.readLine();
+			}
+			
+		}catch(IOException e) {e.getMessage();
+										 e.printStackTrace();}
 	}
 
 }
